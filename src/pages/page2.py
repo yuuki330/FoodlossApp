918ed2af7184f75227c1b35e6ad1c0bec4f9bbff
yuuki330 <katsuyu107@gmail.com>
2023-08-23 15:23:19 +0900

    page2.pyのアップデート

--- src/pages/page2.py
+++ src/pages/page2.py
@@ -4,15 +4,22 @@ import numpy as np
 from PIL import Image
 from ocr import ocr
 from preprocess import get_food_and_price_list
+from pathlib import Path
+import tempfile
 
 st.title("page2")
 
-img_path = st.file_uploader("ファイルアップロード", type=['jpg','png']) # ファイルアップロード
-img=Image.open(img_path)
-img_array = np.array(img)
-st.image(img_array,caption = 'アップロード画像', use_column_width = True)
+uploaded_file = st.file_uploader("レシート画像をアップロード", type=['jpg','png'])
 
-food_price_str = ocr(img_path)
-df = pd.DataFrame(get_food_and_price_list(food_price_str),columns=['Name','Price'])
+if uploaded_file is not None:
+    img=Image.open(uploaded_file)
+    img_array = np.array(img)
+    st.image(img_array,caption = 'アップロード画像', use_column_width = True)
 
-st.table(df)
+    # Make temp file path from uploaded file
+    with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
+        fp = Path(tmp_file.name)
+        fp.write_bytes(uploaded_file.getvalue())
+        food_price_str = ocr(fp)
+        df = pd.DataFrame(get_food_and_price_list(food_price_str),columns=['Name','Price'])
+        st.table(df)
