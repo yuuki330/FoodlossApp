b505332999aa7e3f3d7228dff60fddd4c0ea082f
yuuki330 <katsuyu107@gmail.com>
2023-08-23 16:58:50 +0900

    ファイル整理

--- src/pages/recipe.py
+++ src/pages/recipe.py
@@ -1,52 +1,11 @@
-# import streamlit as st
-# from typing import List
-# import sqlite3 
-
-# def suggest_recipes(food_list: List[str]) -> List[tuple[str, str, str, str, str]]:
-#     conn = sqlite3.connect("../recipe.db")
-#     cur = conn.cursor()
-
-#     execute_order = "SELECT * FROM recipe WHERE "
-#     sub_order = []
-#     for i in range(len(food_list)):
-#         if i:
-#             execute_order += " AND "
-#         execute_order += "recipeMaterial LIKE ?"
-#         sub_order.append('%' + food_list[i] + '%')
-#     sub_order = tuple(sub_order)
-
-#     cur.execute(execute_order, sub_order)
-#     items_list = cur.fetchall()
-
-#     return items_list
-
-# st.markdown("# レシピ検索")
-# recipe_title_list = []
-# recipe_url_list = []
-# recipe_img_url_list = []
-
-# food_list = ['にんじん', 'じゃがいも', 'たまねぎ']
-# items_list = suggest_recipes(food_list=food_list)
-# for v in items_list:
-#     (_, recipeTitle, recipeMaterial, foodImageUrl, recipeUrl) = v
-#     # print(recipeTitle, recipeMaterial, foodImageUrl, recipeUrl)
-#     recipe_title_list.append(recipeTitle)
-#     recipe_url_list.append(recipeUrl)
-#     recipe_img_url_list.append(foodImageUrl)
-
-# for i in range(len(recipe_title_list)):
-#     recipe = '<a href="' + str(recipe_url_list[i]) + '" target="_blank";style="font-family:monospace; color:cyan; font-size: 15px;">' + str(recipe_title_list[i]) + '</a>'
-#     recipe_img = '<img src="' + str(recipe_img_url_list[i]) + '" height="100" width="200">'
-#     st.components.v1.html("<center>" + recipe + "</center>" + "<center>" + recipe_img + "</center>")
-
-
 import streamlit as st
 from typing import List
 import sqlite3
+import pandas as pd
 import sys 
 sys.path.append("../")
 
-def suggest_recipes(food_list: List[str]) -> List[tuple[str, str, str, str, str]]:
+def _suggest_recipes(food_list: List[str]) -> List[tuple[str, str, str, str, str]]:
     try:
         with sqlite3.connect("recipe.db") as conn:
             cur = conn.cursor()
@@ -68,10 +27,28 @@ def suggest_recipes(food_list: List[str]) -> List[tuple[str, str, str, str, str]
         st.error(f"SQLite error: {e}")
         return []
 
-st.markdown("# レシピ検索")
+## ソート系
+# 期限切れ早い順にソート(pandasのdf形式) ＆ 食材上位3つ(list形式)
+def sort_expiration(filepath = "stock.sqlite", limit=3):
+    conn = sqlite3.connect(filepath) 
+    cur = conn.cursor()
+    
+    sql = f"SELECT * FROM stocks ORDER BY expiration_date LIMIT {limit};"
+    df = pd.read_sql(sql, conn)
+    
+    food_list = df.iloc[:,1].values.tolist()
+    
+    return df,food_list
 
+def suggest_recipe(db_name="stock.sqlite"):
+    _, food_list = sort_expiration(db_name)
+    return _suggest_recipes(food_list)
+
+
+## streamlit表示
+st.markdown("# レシピ検索")
 food_list = ['にんじん', 'じゃがいも', 'たまねぎ']
-items_list = suggest_recipes(food_list=food_list)
+items_list = suggest_recipe("stock_japanese.sqlite")
 
 for v in items_list:
     (_, recipeTitle, recipeMaterial, foodImageUrl, recipeUrl) = v
@@ -79,3 +56,4 @@ for v in items_list:
     recipe = f'<a href="{recipeUrl}" target="_blank" style="font-family:monospace; color:cyan; font-size: 15px;">{recipeTitle}</a>'
     recipe_img = f'<img src="{foodImageUrl}" height="100" width="200">'
     st.components.v1.html(f"<center>{recipe}</center><center>{recipe_img}</center>")
+
