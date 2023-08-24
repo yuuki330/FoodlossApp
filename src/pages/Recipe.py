f7836a87e492072ac85b3af6c9f2d6465ca661b8
yuuki330 <katsuyu107@gmail.com>
2023-08-24 16:56:56 +0900

    Change

--- src/pages/Recipe.py
+++ src/pages/Recipe.py
@@ -92,6 +92,6 @@ else:
         with st.container():
             col1, col2 = st.columns([1,1])
             with col1:
-                st.components.v1.html(combined_html) 
+                st.components.v1.html(combined_html, height=300) 
             with col2:
                 st.image(foodImageUrl, use_column_width = "auto")
