236db63aa058c8829b1c4f8e8ca5f54f05e3f3fc
yuuki330 <katsuyu107@gmail.com>
2023-08-24 17:43:15 +0900

    Change button

--- src/pages/Recipe.py
+++ src/pages/Recipe.py
@@ -93,7 +93,8 @@ else:
             col1, col2 = st.columns([1,1])
             with col1:
                 st.components.v1.html(combined_html, height=300) 
-                if hidden_materials and st.button(f"もっと見る"):
+                if hidden_materials and st.button(f"もっと見る", key=recipeTitle):
                     combined_html += hidden_html
+                    st.components.v1.html(combined_html, height=300) 
             with col2:
                 st.image(foodImageUrl, use_column_width = "auto")
