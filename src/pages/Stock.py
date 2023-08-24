0e6955c543594ff2e5d52c45b3c281dc6f038fc9
yuuki330 <katsuyu107@gmail.com>
2023-08-24 10:29:42 +0900

    確認

--- src/pages/Stock.py
+++ src/pages/Stock.py
@@ -14,6 +14,12 @@ from functions import init_stock, add_stock, delete_stock, consume, discard,coun
 current_dir = os.getcwd()
 #"stock.sqlite"のディレクトリパスを取得
 filepath = os.path.join(current_dir, "pages", "data", "stock.sqlite")
+
+# ファイルの存在を確認
+if not os.path.exists(filepath):
+    st.error(f"{filepath} が存在しません。")
+    exit()
+
 st.title("Stock 画面")
 
 with sqlite3.connect(filepath) as conn:
