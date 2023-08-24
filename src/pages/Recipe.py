6399989746fca11d72b250958866f4a6cff7339a
yuuki330 <katsuyu107@gmail.com>
2023-08-24 17:50:50 +0900

    Change Recipe

--- src/pages/Recipe.py
+++ src/pages/Recipe.py
@@ -63,11 +63,7 @@ else:
 
     for v in items_list:
         (_, recipeTitle, recipeMaterial, foodImageUrl, recipeUrl) = v
-
-        # リストの各項目をHTMLの箇条書きに変換
         recipeMaterial = eval(recipeMaterial)
-
-        # 最初に表示する材料数
         initial_display = 5
         displayed_materials = recipeMaterial[:initial_display]
         hidden_materials = recipeMaterial[initial_display:]
@@ -87,16 +83,23 @@ else:
         recipe = f'<a href="{recipeUrl}" target="_blank" style="font-family:monospace; color:cyan; font-size: 18px;">{recipeTitle}</a>'
         material = f'<p target="_blank">{materials_html}</p>'
 
+        # ボタンがクリックされたかどうかの状態を確認
+        button_clicked = st.session_state.get(f"button_clicked_{recipeTitle}", False)
+
+        if not button_clicked and hidden_materials:
+            if st.button(f"もっと見る", key=recipeTitle):
+                st.session_state[f"button_clicked_{recipeTitle}"] = True
+                button_clicked = True
+
+        if button_clicked:
+            material = f'<p target="_blank">{materials_html}{hidden_html}</p>'
+
         combined_html = f"{recipe}<br>{material}"
 
-        if hidden_materials and st.button(f"もっと見る", key=recipeTitle):
-                    combined_html += hidden_html
-                    # st.components.v1.html(combined_html, height=300) 
-                    
         with st.container():
             col1, col2 = st.columns([1,1])
             with col1:
-                st.components.v1.html(combined_html, height=300) 
-                
+                st.components.v1.html(combined_html, height=300)
             with col2:
                 st.image(foodImageUrl, use_column_width = "auto")
+
