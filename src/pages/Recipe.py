110f00049c9bb44915b962926ce446326bad1a36
yuuki330 <katsuyu107@gmail.com>
2023-08-24 18:46:56 +0900

    Change

--- src/pages/Recipe.py
+++ src/pages/Recipe.py
@@ -93,9 +93,9 @@ else:
         # material変数に含まれるHTMLの<li>タグの数をカウント
         material_li_count = material.count('<li>')
         # 1つの<li>タグ項目の高さを設定
-        height_per_li = 30
+        height_per_li = 25
         # 必要な高さを計算
-        calculated_height = material_li_count * height_per_li
+        calculated_height = material_li_count * height_per_li + 50
 
         recipe = f'<a href="{recipeUrl}" target="_blank" style="font-family:monospace; color:cyan; font-size: 13px;">{recipeTitle}</a>'
         combined_html = f"{recipe}<br>{material}"
