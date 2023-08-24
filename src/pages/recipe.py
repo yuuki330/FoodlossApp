b896327b48096f86e7877a9cad691bc300d89c29
yuuki330 <katsuyu107@gmail.com>
2023-08-24 15:07:17 +0900

    change image

--- src/pages/recipe.py
+++ src/pages/recipe.py
@@ -65,7 +65,7 @@ else:
         (_, recipeTitle, recipeMaterial, foodImageUrl, recipeUrl) = v
 
         recipe = f'<a href="{recipeUrl}" target="_blank" style="font-family:monospace; color:cyan; font-size: 15px;">{recipeTitle}</a>'
-        recipe_img = f'<img src="{foodImageUrl}" height="300" width="">'
+        # recipe_img = f'<img src="{foodImageUrl}" height="300" width="">'
+        st.components.v1.html(f"<center>{recipe}</center>")
         st.image(foodImageUrl)
-        # st.components.v1.html(f"<center>{recipe}</center><center>{recipe_img}</center>")
 
